dad1911551108ce1e4f19414a6ac2411a7e6a03c
FernandesMN <fernandes.matheuscp@gmail.com>
2020-02-10 07:16:00 -0300

    challenge 4-3: creating route and form for teacher creation and styling

--- server.js
+++ server.js
@@ -6,13 +6,13 @@ const server = express();
 
 server.set("view engine", "njk");
 
+server.use(express.urlencoded({extended: true}));
 server.use(express.static('public'));
 server.use(routes);
 
 nunjucks.configure("views", {
     express: server,
     autoescape: false,
-    noCache: true
 })
 
 server.listen(5000);
